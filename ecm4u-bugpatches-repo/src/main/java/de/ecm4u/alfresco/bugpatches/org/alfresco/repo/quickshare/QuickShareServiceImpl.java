bb6400828aaf17cbab8262f4648aeadc8e2eb5dd
Lutz Horn <lutz.horn@ecm4u.de>
2016-08-25 13:01:16 +0200

    A NodRef from a tenant is unmangled correctly. Task #2 - Using Multi Tenant, a QuickShare link can only be accessed by users of the tennat the document is in

---
+++ ecm4u-bugpatches-repo/src/main/java/de/ecm4u/alfresco/bugpatches/org/alfresco/repo/quickshare/QuickShareServiceImpl.java
@@ -0,0 +1,171 @@
+/*
+ * #%L
+ * Alfresco Repository
+ * %%
+ * Copyright (C) 2005 - 2016 Alfresco Software Limited
+ * %%
+ * This file is part of the Alfresco software. 
+ * If the software was purchased under a paid Alfresco license, the terms of 
+ * the paid license agreement will prevail.  Otherwise, the software is 
+ * provided under the following open source license terms:
+ * 
+ * Alfresco is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Alfresco is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
+ * #L%
+ */
+package de.ecm4u.alfresco.bugpatches.org.alfresco.repo.quickshare;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.alfresco.repo.tenant.TenantService;
+import org.alfresco.repo.tenant.TenantUtil;
+import org.alfresco.repo.tenant.TenantUtil.TenantRunAsWork;
+import org.alfresco.service.cmr.attributes.AttributeService;
+import org.alfresco.service.cmr.quickshare.InvalidSharedIdException;
+import org.alfresco.service.cmr.repository.NodeRef;
+import org.alfresco.service.cmr.repository.StoreRef;
+import org.alfresco.service.cmr.search.ResultSet;
+import org.alfresco.service.cmr.search.SearchParameters;
+import org.alfresco.service.cmr.search.SearchService;
+import org.alfresco.util.Pair;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ *
+ * @author Lutz Horn
+ * @since 3.0
+ */
+public class QuickShareServiceImpl extends org.alfresco.repo.quickshare.QuickShareServiceImpl {
+
+    private static final Log LOGGER = LogFactory.getLog(QuickShareServiceImpl.class);
+
+    static final String ATTR_KEY_SHAREDIDS_ROOT = ".sharedIds";
+
+    private AttributeService attributeService;
+    private SearchService searchService;
+    private TenantService tenantService;
+
+    @Override
+    public Pair<String, NodeRef> getTenantNodeRefFromSharedId(final String sharedId) {
+        NodeRef nodeRef = TenantUtil.runAsDefaultTenant(new TenantRunAsWork<NodeRef>() {
+            public NodeRef doWork() throws Exception {
+                return (NodeRef) attributeService.getAttribute(ATTR_KEY_SHAREDIDS_ROOT, sharedId);
+            }
+        });
+
+        if (nodeRef == null) {
+            /* TODO
+             * Temporary fix for RA-1093 and MNT-16224. The extra lookup should be
+             * removed (the same as before, just throw the 'InvalidSharedIdException' exception) when we
+             * have a system wide patch to remove the 'shared' aspect of the nodes that have been archived while shared.
+             */
+            // TMDQ
+            final String query = "+TYPE:\"cm:content\" AND +ASPECT:\"qshare:shared\" AND =qshare:sharedId:\"" + sharedId + "\"";
+            SearchParameters sp = new SearchParameters();
+            sp.setLanguage(SearchService.LANGUAGE_FTS_ALFRESCO);
+            sp.setQuery(query);
+            sp.addStore(StoreRef.STORE_REF_WORKSPACE_SPACESSTORE);
+
+            List<NodeRef> nodeRefs = null;
+            ResultSet results = null;
+            try {
+                results = searchService.query(sp);
+                nodeRefs = results.getNodeRefs();
+            } catch (Exception ex) {
+                throw new InvalidSharedIdException(sharedId);
+            } finally {
+                if (results != null) {
+                    results.close();
+                }
+            }
+            if (nodeRefs.size() != 1) {
+                throw new InvalidSharedIdException(sharedId);
+            }
+
+            nodeRef = tenantService.getName(nodeRefs.get(0));
+        }
+
+        // note: relies on tenant-specific (ie. mangled) nodeRef
+        String tenantDomain = tenantService.getDomain(nodeRef.getStoreRef().getIdentifier());
+
+        // PATCH START
+        if (LOGGER.isDebugEnabled()) {
+            LOGGER.debug("tenantDomain=" + tenantDomain);
+        }
+
+        // Here we don't care about the tenant of the current user as in the
+        // original version of this class. We just remove the tenant from the
+        // NodeRef.
+        final NodeRef baseName = unmangleNodeRef(nodeRef);
+        return new Pair<>(tenantDomain, baseName);
+        // PATCH END
+
+    }
+
+    private NodeRef unmangleNodeRef(NodeRef nodeRef) {
+        if (LOGGER.isDebugEnabled()) {
+            LOGGER.debug("nodeRef=" + nodeRef);
+        }
+        String nodeRefAsString = nodeRef.toString();
+        if (LOGGER.isDebugEnabled()) {
+            LOGGER.debug("nodeRefAsString=" + nodeRefAsString);
+        }
+        String[] parts = nodeRefAsString.split(TenantService.SEPARATOR);
+        if (LOGGER.isDebugEnabled()) {
+            LOGGER.debug("parts=" + Arrays.toString(parts));
+        }
+        if (parts.length == 3) {
+            nodeRefAsString = parts[0] + parts[2];
+        }
+        if (LOGGER.isDebugEnabled()) {
+            LOGGER.debug("nodeRefAsString=" + nodeRefAsString);
+        }
+        nodeRef = new NodeRef(nodeRefAsString);
+        if (LOGGER.isDebugEnabled()) {
+            LOGGER.debug("nodeRef=" + nodeRef);
+        }
+        return nodeRef;
+    }
+
+    /**
+     * Set the attribute service
+     *
+     * @param attributeService the attributeService to set
+     */
+    @Override
+    public void setAttributeService(AttributeService attributeService) {
+        this.attributeService = attributeService;
+    }
+
+    /**
+     * Spring configuration
+     *
+     * @param searchService the searchService to set
+     */
+    public void setSearchService(SearchService searchService) {
+        this.searchService = searchService;
+    }
+
+    /**
+     * Set the tenant service
+     *
+     * @param tenantService the tenantService to set
+     */
+    @Override
+    public void setTenantService(TenantService tenantService) {
+        this.tenantService = tenantService;
+    }
+}
